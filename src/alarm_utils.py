2667babed09a4a45003ee5050d0ddfc1c2602c04
Ben Brandt <benbrandt22@gmail.com>
2021-08-10 21:00:14 -0500

    switches mode when buttons B or C pressed

--- src/alarm_utils.py
+++ src/alarm_utils.py
@@ -1,8 +1,17 @@
 import alarm
 import time
+import board
+from digitalio import DigitalInOut, Direction, Pull
 
 # shortcut functions for generating device-wakeup alarms
 
-
 def time_alarm_sec(seconds: int):
     return alarm.time.TimeAlarm(epoch_time=(time.time() + seconds))
+
+def all_button_alarms():
+    """Returns a set of pin alarms that includes all buttons"""
+    return [ pin_alarm_button(board.BUTTON_A), pin_alarm_button(board.BUTTON_B), pin_alarm_button(board.BUTTON_C), pin_alarm_button(board.BUTTON_D) ]
+
+def pin_alarm_button(button_pin):
+    """Returns a pin alarm for the specified button pin. Use 'board.BUTTON_A' through 'D'"""
+    return alarm.pin.PinAlarm(pin=button_pin, value=False, pull=True)
---
+++ src/mlb/message/message_view.py
@@ -0,0 +1,39 @@
+from mlb.models.game_detail import GameDetail
+import time
+from adafruit_display_text import label
+import terminalio
+import board
+import displayio
+
+class MessageView:
+
+    def __init__(self, message:str) -> None:
+        self._message = message
+
+    def render(self):
+        display = board.DISPLAY
+        
+        time.sleep(display.time_to_refresh)
+
+        # main group to hold everything
+        main_group = displayio.Group()
+
+        # white background. Scaled to save RAM
+        bg_bitmap = displayio.Bitmap(display.width // 8, display.height // 8, 1)
+        bg_palette = displayio.Palette(1)
+        bg_palette[0] = 0xFFFFFF
+        bg_sprite = displayio.TileGrid(bg_bitmap, x=0, y=0, pixel_shader=bg_palette)
+        bg_group = displayio.Group(scale=8)
+        bg_group.append(bg_sprite)
+        main_group.append(bg_group)
+
+        # Message label
+        message_label = label.Label(terminalio.FONT, text=self._message, color=0x000000)
+        message_label.anchor_point = (0.5, 0.5)
+        message_label.anchored_position = (display.width // 2, display.height // 2)
+        main_group.append(message_label)
+
+        # show the main group and refresh.
+        display.show(main_group)
+        display.refresh()
+
