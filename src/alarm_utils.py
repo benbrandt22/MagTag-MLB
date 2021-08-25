c53cb7ca6daed4445431a134097a157e684989ea
Ben Brandt <benbrandt22@gmail.com>
2021-08-25 18:46:07 -0500

    Added helper methods for alarm wakeup logic

--- src/alarm_utils.py
+++ src/alarm_utils.py
@@ -15,3 +15,17 @@ def all_button_alarms():
 def pin_alarm_button(button_pin):
     """Returns a pin alarm for the specified button pin. Use 'board.BUTTON_A' through 'D'"""
     return alarm.pin.PinAlarm(pin=button_pin, value=False, pull=True)
+
+def was_woken_by_pin(button_pin):
+    """Returns true if the device was woken by the specified pin"""
+    return ( alarm.wake_alarm is not None \
+        and type(alarm.wake_alarm) is alarm.pin.PinAlarm \
+        and alarm.wake_alarm.pin == button_pin )
+
+def was_woken_by_time():
+    """Returns true if the device was woken by a TimeAlarm"""
+    return ( alarm.wake_alarm is not None and type(alarm.wake_alarm) is alarm.time.TimeAlarm )
+
+def was_woken_by_powerup():
+    """Returns true if the device was turned on, and not woken by any alarm"""
+    return ( alarm.wake_alarm is None )
